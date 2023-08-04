319edea2d9b552f07d5161c85f20f9e4d9060422
FabForReal <brzzzn2005@gmail.com>
2023-08-04 17:20:59 +0200

    base implementation done

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # KeepInventory Penalty
- Minecraft Fabric Mod
+
+**KeepInventory Penalty** adds an XP Penalty when you die while using keepInventory
+
+Feel free to learn from this repository or use it to develop your own mod.
+
+## Download 📖
+
+- Download the latest stable release under the "Releases" section here
+- *Modrinth Soon*
+
+## FAQ 📖
+
+##### Can I include this in my Modpack?
+- Yes.
+
+##### Backport to X? 
+- Unlikely.
+
+##### Forge?
+- No.
+
+##### Is this mod compatible with X?
+- I don't know. I will add a list of incompatible mods if problems come to my attention.
--- src/main/java/fab/keepinventorypenalty/mixin/DeathMixin.java
+++ src/main/java/fab/keepinventorypenalty/mixin/DeathMixin.java
@@ -1,6 +1,7 @@
 package fab.keepinventorypenalty.mixin;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,10 +10,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class DeathMixin
 {
-	// Unsure whether injecting at the top or bottom of the method is better
+	// Move this to the config file later
+	private final int lossPercentage = 2;
+
+	// Unsure whether injecting at the top or bottom (iykyk) of the method is better
 	@Inject(at = @At("TAIL"), method = "onDeath")
 	private void init(CallbackInfo info)
 	{
+		// cast triggering player
+		ServerPlayerEntity instance = (ServerPlayerEntity) (Object) this;
 
+		if(instance.getWorld().getGameRules().getBoolean(GameRules.KEEP_INVENTORY))
+		{
+			int totalLoss = instance.experienceLevel / lossPercentage;
+			instance.setExperienceLevel(instance.experienceLevel - totalLoss);
+		}
 	}
 }
